3897273578e19525c8a1814745634d6b8f31e2dd
BoyK07 <boy.modusdigital@gmail.com>
2024-09-24 14:55:23 +0200

    Deployer

---
+++ deploy.php
@@ -0,0 +1,23 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Retrieve the server IP from environment variable
+$vpsIP = getenv('VPS_IP');
+
+// Server configuration
+host($vpsIP)
+    ->set('remote_user', 'boy')
+    ->set('port', 4000)
+    ->set('deploy_path', '/home/boy/websites/portfolio');
+
+// Define deployment tasks
+task('startsite', function () {
+    run('cd {{deploy_path}}/current && composer install && docker compose up -d');
+});
+
+// Hooks
+after('deploy:symlink', 'startsite'); // This runs after the code has been deployed
+after('deploy:failed', 'deploy:unlock'); // Unlock on failure
+after('deploy:success', 'deploy:unlock'); // Unlock on success
