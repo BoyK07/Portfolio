da3db5fb9996403eafdf832aaa1bc8821394bb42
BoyK07 <boy.modusdigital@gmail.com>
2024-09-24 14:58:51 +0200

    deployer

--- deploy.php
+++ deploy.php
@@ -3,18 +3,19 @@ namespace Deployer;
 
 require 'recipe/laravel.php';
 
-// Retrieve the server IP from environment variable
+// Retrieve the server IP from the environment variable
 $vpsIP = getenv('VPS_IP');
 
 // Server configuration
-host($vpsIP)
+host('production') // Name the host 'production'
+    ->set('hostname', $vpsIP)  // Use the IP from environment variables
     ->set('remote_user', 'boy')
     ->set('port', 4000)
     ->set('deploy_path', '/home/boy/websites/portfolio');
 
 // Define deployment tasks
 task('startsite', function () {
-    run('cd {{deploy_path}}/current && composer install && docker compose up -d');
+    run('cd {{deploy_path}}/current && composer install && docker-compose up -d');
 });
 
 // Hooks
