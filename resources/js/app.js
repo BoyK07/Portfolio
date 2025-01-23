a9f90d8f124e048e94c067b362b8bb1922983ea8
BoyK07 <boy.krijnen@ziggo.nl>
2025-01-23 11:44:34 +0100

    Add tsparticles-preset-stars dependency and implement Typed.js for dynamic language display

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,15 @@
 import './bootstrap';
-
 import Alpine from 'alpinejs';
-
 window.Alpine = Alpine;
-
 Alpine.start();
+
+// Typed.js
+import Typed from 'typed.js';
+new Typed('#languages', {
+    strings: ['PHP', 'Javascript', 'Python', 'Laravel', 'Java', 'Docker', 'MySQL', 'Linux'],
+    typeSpeed: 50,
+    shuffle: true,
+    backDelay: 1000,
+    backSpeed: 50,
+    loop: true,
+});
